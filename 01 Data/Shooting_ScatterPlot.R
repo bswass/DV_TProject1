64da36adc6274167d8199682ca6b98a70815ef6f
German MartinezGarcia <germanmtz93@gmail.com>
2015-11-11 16:49:46 -0600

    added the peeped files to the repo (along with reference folder)

---
+++ 01 Data/Shooting_ScatterPlot.R
@@ -0,0 +1,9 @@
+require("jsonlite")
+require("RCurl")
+require(ggplot2)
+require(dplyr)
+
+
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+                                                "select * from shootingsmass"')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_btb687', PASS='orcl_btb687', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE)))
+
---
+++ 01 Data/referenceFolder/BarChart.R
@@ -0,0 +1,71 @@
+require("jsonlite")
+require("RCurl")
+require(ggplot2)
+require(dplyr)
+
+# The following is equivalent to KPI Story 2 Sheet 2 and Parameters Story 3 in "Crosstabs, KPIs, Barchart.twb"
+
+# These will be made to more resemble Tableau Parameters when we study Shiny.
+KPI_Low_Max_value = 4750     
+KPI_Medium_Max_value = 5000
+
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+"select color, clarity, avg_price, 
+avg(avg_price) 
+OVER (PARTITION BY clarity ) as window_avg_price
+from (select color, clarity, avg(price) avg_price
+   from diamonds
+   group by color, clarity)
+order by clarity;"
+')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_UTEid', PASS='orcl_UTEid', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); View(df)
+
+# df <- diamonds %>% group_by(color, clarity) %>% summarize(AVG_PRICE = mean(price)) %>% rename(COLOR=color, CLARITY=clarity)
+# df1 <- df %>% ungroup %>% group_by(CLARITY) %>% summarize(WINDOW_AVG_PRICE=mean(AVG_PRICE))
+# df <- inner_join(df, df1, by="CLARITY")
+
+#spread(df, COLOR, AVG_PRICE) %>% View
+
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  facet_wrap(~CLARITY, ncol=1) +
+  labs(title='Diamonds Barchart\nAVERAGE_PRICE, WINDOW_AVG_PRICE, ') +
+  labs(x=paste("COLOR"), y=paste("AVG_PRICE")) +
+  layer(data=df, 
+        mapping=aes(x=COLOR, y=AVG_PRICE), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="bar",
+        geom_params=list(colour="blue"), 
+        position=position_identity()
+  ) + coord_flip() +
+  layer(data=df, 
+        mapping=aes(x=COLOR, y=AVG_PRICE, label=round(AVG_PRICE)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", hjust=-0.5), 
+        position=position_identity()
+  ) +
+  layer(data=df, 
+        mapping=aes(x=COLOR, y=AVG_PRICE, label=round(WINDOW_AVG_PRICE)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", hjust=-2), 
+        position=position_identity()
+  ) +
+  layer(data=df, 
+        mapping=aes(x=COLOR, y=AVG_PRICE, label=round(AVG_PRICE - WINDOW_AVG_PRICE)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", hjust=-5), 
+        position=position_identity()
+  ) +
+  layer(data=df, 
+        mapping=aes(yintercept = WINDOW_AVG_PRICE), 
+        geom="hline",
+        geom_params=list(colour="red")
+  ) 
